aa0e7cee8c0d836424b552f8bf63793d773ee4af
Aarti Aggarwal <aartiaggarwal019@gmail.com>
2024-09-03 22:28:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/home";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import User from "./components/User";
+import NotFound from "./components/NotFound";
+import AlertState from './context/alert/AlertState';
+import GithubState from "./context/github/GithubState";
+
+// process.setMaxListeners(20); // Increase the limit as needed
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div>
+            <Navbar title="Github Finder" icon="fab fa-github" />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} /> {/* Fixed 'component' prop */}
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+}
+
+export default App;
